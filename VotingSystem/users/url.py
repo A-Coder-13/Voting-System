0474d6e0b605c9e673d3e78ee3fc1f78e4bff839
HMY  "The Coder <209747721+A-Coder-13@users.noreply.github.com>
2026-03-14 17:00:51 +0500

    Add password recovery URLs to user routes

--- VotingSystem/users/url.py
+++ VotingSystem/users/url.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import reg, user_login,user_logout
+from .views import reg, user_login,user_logout,forget_password,otp,reset_password
 
 urlpatterns=[
     path('regist',reg,name='regis'),
     path('login',user_login,name='login'),
     path('logout',user_logout,name='Logout'),
+    path('forget_password',forget_password,name='forget_password'),
+    path('otp',otp,name='OTP'),
+    path('reset_password',reset_password,name='reset_password'),
 ]
